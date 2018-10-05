d26c12f56e079be35a5418eda8e8efda4df310a3
DESKTOP-JTOUO4J\Stuart <strt.mcroberts@gmail.com>
2018-10-06 09:43:32 +1000

    Initial Commit for project
    Initial Commit with Unity Project setup

---
+++ TechTest/Assets/Code/Core/Mediator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Stuart McRoberts
+//Messaging system to communicate between different projects
+public class Mediator : MonoBehaviour
+{
+    public static Mediator Instance = null;
+
+
+    public static Dictionary<object, List<CommunicationHandler>> RegisteredHandlers = new Dictionary<object, List<CommunicationHandler>>();
+
+
+
+    public static Dictionary<object, List<object>> RegisteredObjects = new Dictionary<object, List<object>>();
+
+    public delegate void CommunicationHandler(Message message);
+    private void Awake()
+    {
+        Debug.Log(this.transform.name);
+        if (Mediator.Instance != null)
+        {
+            Mediator.Instance = null;
+            Mediator.Instance = this;
+
+        }
+        else
+        {
+            Mediator.Instance = this;
+        }
+    }
+
+    public static void SendMessage(object Identification, params object[] args)
+    {
+        Message msg = new Message();
+        if (args.Length > 1)
+        {
+
+            msg.CreatePayload(args);
+
+        }
+        if (RegisteredHandlers.ContainsKey(Identification))
+        {
+            foreach (CommunicationHandler handler in RegisteredHandlers[Identification])
+            {
+                handler(msg);
+            }
+        }
+    }
+
+    public static void RegisterHandler(object Identification, object baseObject, CommunicationHandler funct)
+    {
+        if (RegisteredHandlers.ContainsKey(Identification))
+        {
+            RegisteredHandlers[Identification].Add(funct);
+        }
+        else
+        {
+            RegisteredHandlers.Add(Identification, new List<CommunicationHandler>());
+            RegisteredHandlers[Identification].Add(funct);
+        }
+        if (RegisteredObjects.ContainsKey(baseObject))
+        {
+            RegisteredObjects[baseObject].Add(Identification);
+        }
+        else
+        {
+            RegisteredObjects.Add(baseObject, new List<object>());
+            RegisteredObjects[baseObject].Add(Identification);
+        }
+
+
+    }
+    public class CommunicationPair
+    {
+        public object key;
+        public object value;
+
+
+    }
+
+
+}
---
+++ TechTest/Assets/Code/Core/Message.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+
+
+public class Message
+{
+    public Hashtable Properties = new Hashtable();
+
+    public object this[object value]
+    {
+        get { return Properties[value]; }
+    }
+
+    public Message CreatePayload(params object[] args)
+    {
+        if (args.Length > 0)
+        {
+            Properties = new Hashtable();
+
+            int counter = 0;
+
+            if ((args.Length % 2) == 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (counter == i && i > 0)
+                    {
+                        counter = 0;
+                    }
+                    else
+                    {
+                        Properties.Add(args[i], args[i + 1]);
+                        counter = i + 1;
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return this;
+    }
+
+
+}
+
